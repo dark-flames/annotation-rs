0547765cc9663934b4180f6c3b1317823211d528
dark-flames <dark_flames@outlook.com>
2020-06-03 11:43:40 +0900

    test: full test

---
+++ yui_test/tests/full_test.rs
@@ -0,0 +1,33 @@
+#![feature(proc_macro_hygiene)]
+use yui::get_attribute;
+use yui_test::FullDerive;
+use yui_test_attribute::attribute::Simple;
+use yui_test_attribute::attribute::Full;
+use yui_test_attribute::enums::TestEnum;
+use std::iter::FromIterator;
+
+
+#[derive(FullDerive)]
+#[Full(
+    object(i32 = 1, u16 = 2, float = 1.1, string = "test", enum2 = "aaa"),
+    vector("1", "2", "3"),
+    map(a="aaa", b="variant_b", c="variant_c")
+
+)]
+struct Test;
+
+#[test]
+pub fn test_full() {
+    let attr: Full = get_attribute!(Test, Full);
+    let simple = attr.object;
+    assert_eq!(simple.int32, 1);
+    assert_eq!(simple.unsigned16, 2);
+    assert_eq!(simple.float, 1.1);
+    assert_eq!(simple.string, "test");
+    assert_eq!(simple.enum1, Some(TestEnum::VariantC));
+    assert_eq!(simple.enum2, TestEnum::VariantA);
+    assert_eq!(attr.vector, ["1", "2", "3"]);
+    assert_eq!(attr.map["a"], TestEnum::VariantA);
+    assert_eq!(attr.map["b"], TestEnum::VariantB);
+    assert_eq!(attr.map["c"], TestEnum::VariantC);
+}
