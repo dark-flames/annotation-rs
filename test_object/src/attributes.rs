22642e5e86bdf38e1382421df8ae3627b3b0f524
dark-flames <dark_flames@outlook.com>
2021-01-06 06:58:49 +0900

    chore: specific version

---
+++ test_object/src/attributes.rs
@@ -0,0 +1,39 @@
+use crate::enums::TestEnum;
+use std::collections::HashMap;
+
+use derive::Annotation;
+
+#[derive(Annotation, Clone)]
+#[mod_path = "test_object::attributes"]
+pub struct NoField;
+
+#[derive(Annotation, Clone)]
+#[mod_path = "test_object::attributes"]
+pub struct Simple {
+    #[field(alias = "i32")]
+    pub int32: i32,
+    #[field(alias = "u16")]
+    pub unsigned16: u16,
+    pub float: f32,
+    pub string: String,
+    #[field(enum_value = true, default = "variant_c")]
+    pub enum1: Option<TestEnum>,
+    #[field(enum_value = true)]
+    pub enum2: TestEnum,
+}
+
+#[derive(Annotation, Clone)]
+#[mod_path = "test_object::attributes"]
+pub struct Tuple(pub Option<String>);
+
+#[derive(Annotation, Clone)]
+#[mod_path = "test_object::attributes"]
+pub struct Full {
+    pub object: Simple,
+    pub vector: Vec<String>,
+    #[field(enum_value = true)]
+    pub map: HashMap<String, TestEnum>,
+    pub map2: HashMap<String, Tuple>,
+    #[field(enum_value = true)]
+    pub map3: HashMap<String, Vec<TestEnum>>,
+}
---
+++ test_object/src/enums.rs
@@ -0,0 +1,20 @@
+use derive::AnnotationEnumValue;
+
+#[cfg(test)]
+use std::str::FromStr;
+
+#[derive(AnnotationEnumValue, Debug, PartialEq, Clone)]
+#[mod_path = "test_object::enums"]
+pub enum TestEnum {
+    #[variant_value("aaa")]
+    VariantA,
+    VariantB,
+    VariantC,
+}
+
+#[test]
+pub fn test_enum() {
+    assert_eq!(TestEnum::from_str("aaa").unwrap(), TestEnum::VariantA);
+    assert_eq!(TestEnum::from_str("variant_b").unwrap(), TestEnum::VariantB);
+    assert_eq!(TestEnum::from_str("variant_c").unwrap(), TestEnum::VariantC);
+}
